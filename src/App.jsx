5119a7b88a4e2f6d9cab3588b44503b95799d6e3
Isaac025 <isaacolayiwola85@gmail.com>
2025-05-19 07:40:37 -0700

    refractor: changed verify email styles

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ const Verification = lazy(() => import("./pages/Verification"));
 const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
 const CheckYourEmail = lazy(() => import("./pages/CheckYourEmail"));
 import VerifyEmail from "./pages/VerifyEmail";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -28,8 +29,14 @@ function App() {
       <Suspense fallback={<SuspenseLoader />}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<HomeLoggedIn />} />
-          <Route path="/property/:propertyId" element={<PropertyDetail />} />
+
+          <Route
+            element={<ProtectedRoute allowedRoles={["tenant", "landlord"]} />}
+          >
+            <Route path="/home" element={<HomeLoggedIn />} />
+            <Route path="/property/:propertyId" element={<PropertyDetail />} />
+          </Route>
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/verification" element={<Verification />} />
@@ -37,11 +44,13 @@ function App() {
           <Route path="forgot-password" element={<ForgotPassword />} />
           <Route path="/check-your-email" element={<CheckYourEmail />} />
 
-          <Route path="/dashboard" element={<DashboardLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="property" element={<AdminProperty />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="create" element={<CreateProperty />} />
+          <Route element={<ProtectedRoute allowedRoles={["landlord"]} />}>
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="property" element={<AdminProperty />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="create" element={<CreateProperty />} />
+            </Route>
           </Route>
 
           {/* 404 route last */}
