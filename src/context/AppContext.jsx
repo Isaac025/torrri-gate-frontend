596c7f871b850bcca72924a5bb25490aa53b1c39
Isaac025 <isaacolayiwola85@gmail.com>
2025-05-26 04:04:59 -0700

    feat: added profile update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -31,8 +31,13 @@ const AppProvider = ({ children }) => {
     localStorage.removeItem("user");
   };
 
+  const updateUser = (updatedUser) => {
+    setUser(updatedUser);
+    localStorage.setItem("user", JSON.stringify(updatedUser));
+  };
+
   return (
-    <appContext.Provider value={{ login, logout, user, token, loading }}>
+    <appContext.Provider value={{ login, logout, user, token, loading, updateUser }}>
       {children}
     </appContext.Provider>
   );
