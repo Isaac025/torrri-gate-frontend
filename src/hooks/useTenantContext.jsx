bfedcbb2b70bcdcb75d81548c1b271eb5c30f3fc
Isaac025 <isaacolayiwola85@gmail.com>
2025-05-29 07:54:09 -0700

    refractor: FeaturedPropertyCard Component

--- src/hooks/useTenantContext.jsx
+++ src/hooks/useTenantContext.jsx
@@ -1,4 +1,4 @@
 import { useContext } from "react";
 import { TenantContext } from "../context/TenantContext";
 
-export const useAppContext = () => useContext(TenantContext);
+export const useTenantContext = () => useContext(TenantContext);
